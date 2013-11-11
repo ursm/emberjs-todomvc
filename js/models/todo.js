e25090a720f86845740796e14c6a2647c66ffc9d
Keita Urashima <ursm@ursm.jp>
2013-11-12 04:37:11 +0900

    Highlight outdated todos

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,5 +1,13 @@
 Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean'),
-  deadline: DS.attr('string')
+  deadline: DS.attr('string'),
+
+  isOutdated: function() {
+    var deadline = this.get('deadline');
+
+    if (Ember.isEmpty(deadline)) return false;
+
+    return new Date() > new Date(deadline)
+  }.property('deadline')
 });
