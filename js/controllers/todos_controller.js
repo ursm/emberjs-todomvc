151fc708b9654bfe67c4b2ccd571fbc0b031495f
Keita Urashima <ursm@ursm.jp>
2013-11-12 03:23:05 +0900

    finished

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -13,8 +13,23 @@ Todos.TodosController = Ember.ArrayController.extend({
       this.set('newTitle', '');
 
       todo.save();
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
+
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
 
   remaining: function() {
     return this.filterBy('isCompleted', false).get('length');
@@ -24,5 +39,16 @@ Todos.TodosController = Ember.ArrayController.extend({
     var remaining = this.get('remaining');
 
     return remaining === 1 ? 'item' : 'items';
-  }.property('remaining')
+  }.property('remaining'),
+
+  allAreDone: function(key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.everyBy('isCompleted', true);
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+
+      return value;
+    }
+  }.property('@each.isCompleted')
 });
